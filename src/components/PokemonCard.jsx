bdd4da77418af8f77847fe32afc74fd573af41ab
Mamou Skn <mamou.test@gmail.com>
2023-08-22 16:48:02 +0200

    Ajout du figcaption

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -20,18 +20,18 @@ return (
     //     <img src={pokemon.imgSrc} />
     // </div>;
 
+    // il faut toujours mettre des balises figures pour les images !
+
     <div>
-        <figure>
+        <figure>   
         {pokemon.imgSrc === undefined ? "???" : <img src={pokemon.imgSrc} alt={pokemon.name}/>}
         </figure>
         <h1>{pokemon.name}</h1>
+        <figcaption>Bulbasaur</figcaption>
+
+        
         
     </div>
-
-/* <figure>
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Img of bulbasaur." />
-  <figcaption>Bulbasaur</figcaption>
-</figure> */
 
 )
 }
