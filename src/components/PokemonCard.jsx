5ff57436f649bbb13a122cb43f628fee3d5cbc1f
Mamou Skn <mamou.test@gmail.com>
2023-08-22 16:45:42 +0200

    Challenge quêtes 3 affichage pokemon List opération ternaire

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,40 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
 function PokemonCard()  {
-return (<figure>
+    const pokemon = pokemonList[0];
+    console.log(pokemon.imgSrc)
+
+return (
+    // <div>
+    //     <h1>{pokemon.name}</h1>
+    //     <img src={pokemon.imgSrc} />
+    // </div>;
+
+    <div>
+        <figure>
+        {pokemon.imgSrc === undefined ? "???" : <img src={pokemon.imgSrc} alt={pokemon.name}/>}
+        </figure>
+        <h1>{pokemon.name}</h1>
+        
+    </div>
+
+/* <figure>
   <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Img of bulbasaur." />
   <figcaption>Bulbasaur</figcaption>
-</figure>
+</figure> */
+
 )
 }
+
 
 export default PokemonCard;
